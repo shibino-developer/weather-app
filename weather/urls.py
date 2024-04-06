17729d6c791c5f73627cac4beeaae794e090ea95
shibino-developer <shibino.developer@gmail.com>
2024-04-06 18:28:52 +0530

    first commit

---
+++ weather/urls.py
@@ -0,0 +1,7 @@
+# weather/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.weather, name='weather'),
+]
---
+++ weather/views.py
@@ -0,0 +1,17 @@
+# weather/views.py
+from django.shortcuts import render
+import requests
+
+def weather(request):
+    city = request.GET.get('city')
+    if city:
+        api_key = '84b15b6200a9ba9ab7015b2730a57b5a'
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+        response = requests.get(url)
+        weather_data = response.json()
+        return render(request, 'weather/weather.html', {'weather_data': weather_data})
+    else:
+        return render(request, 'weather/weather.html')
+
+
+# Create your views here.
