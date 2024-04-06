70a7eb8041574d647e43fc6062206e50a215b64a
shibino-developer <shibino.developer@gmail.com>
2024-04-06 19:15:47 +0530

    Implemented forecasting

--- django_weather_app/settings.py
+++ django_weather_app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+# Define the directories where Django will look for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- weather/views.py
+++ weather/views.py
@@ -9,9 +9,22 @@ def weather(request):
         url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
         response = requests.get(url)
         weather_data = response.json()
+        
         return render(request, 'weather/weather.html', {'weather_data': weather_data})
     else:
         return render(request, 'weather/weather.html')
 
 
 # Create your views here.
+# views.py
+def forecast(request):
+    city = request.GET.get('city')
+    if city:
+        api_key = '84b15b6200a9ba9ab7015b2730a57b5a'
+        url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric'
+        response = requests.get(url)
+        forecast_data = response.json()
+        print(forecast_data)
+        return render(request, 'weather/forecast.html', {'forecast_data': forecast_data})
+    else:
+        return render(request, 'weather/forecast.html')
